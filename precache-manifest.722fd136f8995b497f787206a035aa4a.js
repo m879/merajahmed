d03ddb139c6e573e1d1540ff12df866f08b33053
m879 <merajahmed2112@gmail.com>
2020-07-12 11:40:33 +0530

    Updates

---
+++ precache-manifest.722fd136f8995b497f787206a035aa4a.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e34b91ac21c857815b6440fc298547b1",
+    "url": "/merajahmed/index.html"
+  },
+  {
+    "revision": "984634d1e422f598b3d2",
+    "url": "/merajahmed/static/css/2.14776d5b.chunk.css"
+  },
+  {
+    "revision": "d15f3a229ec0692f61ad",
+    "url": "/merajahmed/static/css/main.479828a3.chunk.css"
+  },
+  {
+    "revision": "984634d1e422f598b3d2",
+    "url": "/merajahmed/static/js/2.13a5ee64.chunk.js"
+  },
+  {
+    "revision": "a5f655d830d088aa9894c35a94634b59",
+    "url": "/merajahmed/static/js/2.13a5ee64.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d15f3a229ec0692f61ad",
+    "url": "/merajahmed/static/js/main.87939d39.chunk.js"
+  },
+  {
+    "revision": "d9037bae68b1113f3a53",
+    "url": "/merajahmed/static/js/runtime-main.51744eba.js"
+  },
+  {
+    "revision": "52102d5378efc07f427a093345a122c7",
+    "url": "/merajahmed/static/media/blog1.52102d53.jpg"
+  },
+  {
+    "revision": "f989cd159783117a8cc6c81048b1b9ab",
+    "url": "/merajahmed/static/media/img2.f989cd15.png"
+  },
+  {
+    "revision": "9bc799a335ca0aee3daaa7d81deb4090",
+    "url": "/merajahmed/static/media/img3.9bc799a3.png"
+  },
+  {
+    "revision": "444972b9141a4f07e61bb27538d9d60f",
+    "url": "/merajahmed/static/media/meraj.444972b9.png"
+  }
+]);
