fa687b2ee73714482bbd93bed33fcc5f4f38e500
Meraj.Ahmed <merajahmed2112@gmail.com>
2021-10-20 20:02:37 +0530

    minor change

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
  
 import Home from './components/Home';
@@ -8,7 +9,6 @@ import Projects from './components/Projects';
 import Contact from './components/Contact';
 import Error from './components/Error';
 import Navigation from './components/Navigation';
-import 'bootstrap/dist/css/bootstrap.min.css';
 
 class App extends Component {
   render() {
